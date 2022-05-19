305e93e2c5f5c729b31eb764b316fe48dbcf34dd
wudueuk <wudueuk@gmail.com>
2022-05-19 11:34:41 +0200

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import style from './Header.module.css';
+import Layout from '../Layout';
+import Logo from './Logo';
+import Search from './Search';
+import Auth from './Auth';
+import Heading from './Heading';
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+      <Layout>
+        <div className={style.gridContainer}>
+          <Logo />
+          <Heading text='Blogget' />
+          <Search />
+          <Auth auth={false} />
+        </div>
+      </Layout>
+    </header>
+  );
+}
