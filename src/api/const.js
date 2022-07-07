daaf769ef19a8371f707d980e1b469427c692066
wudueuk <wudueuk@gmail.com>
2022-07-07 10:46:09 +0200

    fix const REDIRECT_URI

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'H9HCyks9mywXkJyLM9XiTg';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://methed-blogget.vercel.app/auth';
 export const SCOPE = 'identity submit read';
